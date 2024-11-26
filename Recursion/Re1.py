90437430e1c14a2bb7015b5abe5fe29239c3f072
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-11-26 12:07:08 +0530

    Update Re1.py

--- Recursion/Re1.py
+++ Recursion/Re1.py
@@ -1,11 +1,18 @@
-class Solution:
+# class Solution:
       
-    def printNos(self,N):
-        #Your code here
-        if N == 0:
-            return
-        self.printNos(N-1)
-        print(N, end=" ")
+#     def printNos(self,N):
+#         if N == 0:
+#             return
+#         self.printNos(N-1)
+#         print(N, end=" ")
     
-sol1 = Solution()
-sol1.printNos(10)
+# sol1 = Solution()
+# sol1.printNos(10)
+
+def printNos(n):
+    if n==0:
+        return 
+    printNos(n-1)
+    print(n, end=" ")
+
+printNos(5)
