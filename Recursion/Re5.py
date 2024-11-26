f0dbe5f4955e47355c347f79073cc44f261ec955
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-11-26 15:49:09 +0530

    parameterized way

--- Recursion/Re5.py
+++ Recursion/Re5.py
@@ -1,8 +1,19 @@
 # sum of first N natural numbers
 
-def sumNos(n):
-    if n==0:
-        return 0
-    return n+sumNos(n-1)
 
-print(sumNos(5))
+# Functional way
+# def sumNos(n):
+#     if n==0:
+#         return 0
+#     return n+sumNos(n-1)
+
+# print(sumNos(5))
+
+# Parameterized way
+def sumNos(i, sum):
+    if(i<1):
+        print(sum)
+        return
+    sumNos(i-1, sum+i)
+
+sumNos(5, 0)
