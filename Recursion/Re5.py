cec7445aa1487193b06dce35af3ffacbabc7d49a
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-11-26 16:22:45 +0530

    Update Re5.py

--- Recursion/Re5.py
+++ Recursion/Re5.py
@@ -5,7 +5,14 @@
 # def sumNos(n):
 #     if n==0:
 #         return 0
-#     return n+sumNos(n-1)
+#     return n+sumNos(n-1) # n + f(n-1)
+
+'''
+example: f(3) -> 3 + f(2)
+         f(2) -> 2 + f(1)
+         f(1) -> 1 + f(0)
+         f(0) -> 0
+'''
 
 # print(sumNos(5))
 
