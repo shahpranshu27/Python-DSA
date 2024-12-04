50e1205f6b1f9d03e3d68cea0d11f1e0f64dca86
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-12-04 11:15:10 +0530

    using recursion

--- Recursion/Re7.py
+++ Recursion/Re7.py
@@ -4,11 +4,23 @@ def printArr(arr):
     for i in range(0, len(arr)):
         print(arr[i], end=" ")
 
-def reverseArr(arr):
-    arr.reverse()
+# def reverseArr(arr):
+#     arr.reverse()
 
-arr = [1,2,3]
+# arr = [1,2,3]
+# printArr(arr)
+# reverseArr(arr)
+# print("\n")
+# printArr(arr)
+
+def reverseArr(arr, start, end):
+    if start<end:
+        arr[start], arr[end] = arr[end], arr[start]
+        reverseArr(arr, start+1, end-1)
+
+arr = [1,2,4,5,3]
 printArr(arr)
-reverseArr(arr)
-print("\n")
+n = len(arr)
+reverseArr(arr, 0, n-1)
+print('\n')
 printArr(arr)
