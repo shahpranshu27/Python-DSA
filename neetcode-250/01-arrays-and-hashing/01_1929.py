fb13ee01f1414aa750d7fcb3602517a80c60415f
shahpranshu27 <shahpranshu2712@gmail.com>
2026-07-04 14:57:04 +0530

    01 - 1929 - concatenation of array - new optimized method

--- neetcode-250/01-arrays-and-hashing/01_1929.py
+++ neetcode-250/01-arrays-and-hashing/01_1929.py
@@ -5,7 +5,7 @@ from typing import List
 
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        # ans = nums* 2
+        # ans = 2 * nums
         # return ans
 
         # ans = []
@@ -14,8 +14,24 @@ class Solution:
         #         ans.append(num)
         # return ans
 
-        n = len(nums)
-        ans = [0] * 2*n
-        for i, num in enumerate(nums):
-            ans[i] = ans[i+n] = num
+        # n = len(nums)
+        # ans = [0] * 2*n
+        # for i, num in enumerate(nums):
+        #     ans[i] = ans[i+n] = num
+        # return ans
+
+        # return 2*nums
+
+        # ans = []
+        # for index in range(2):
+        #     for i, num in enumerate(nums):
+        #         ans.insert(i, num)
+        
+        # return ans
+
+        ans = []
+        for _ in range(2):
+            for num in nums:
+                ans.append(num)
+        
         return ans
